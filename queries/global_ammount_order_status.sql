38f3beefdc6ac39a5bc55005efacdf261c460ccf
Duvis07 <duvan.botero@est.iudigital.edu.co>
2025-03-24 16:18:13 -0500

    Guardando cambios antes de cambiar a angelo

--- queries/global_ammount_order_status.sql
+++ queries/global_ammount_order_status.sql
@@ -1,3 +1,12 @@
 -- TODO: Esta consulta devolverá una tabla con dos columnas: estado_pedido y
 -- Cantidad. La primera contendrá las diferentes clases de estado de los pedidos,
 -- y la segunda mostrará el total de cada uno.
+
+SELECT
+	order_status AS order_status,
+	count(olist_orders.order_status) as Amount
+FROM
+	olist_orders
+GROUP BY
+	order_status ;
+
--- queries/revenue_per_state.sql
+++ queries/revenue_per_state.sql
@@ -2,3 +2,21 @@
 -- La primera contendrá las abreviaturas que identifican a los 10 estados con mayores ingresos,
 -- y la segunda mostrará el ingreso total de cada uno.
 -- PISTA: Todos los pedidos deben tener un estado "delivered" y la fecha real de entrega no debe ser nula.
+
+SELECT
+    oc.customer_state AS customer_state,
+    SUM(oop.payment_value) AS Revenue
+FROM
+    olist_orders oo
+JOIN
+    olist_customers oc ON oo.customer_id = oc.customer_id
+JOIN
+    olist_order_payments oop ON oo.order_id = oop.order_id
+WHERE
+    oo.order_status = 'delivered'
+    AND oo.order_delivered_customer_date IS NOT NULL
+GROUP BY
+    oc.customer_state
+ORDER BY
+    Revenue DESC
+LIMIT 10;
